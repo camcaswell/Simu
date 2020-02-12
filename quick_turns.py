a797b8ae24d25f5a2581774772061c2f7b89f387
Unknown <camcaswell@gmail.com>
2020-02-12 16:21:10 -0500

    Add flee_1 and flee_2

--- quick_turns.py
+++ quick_turns.py
@@ -44,7 +44,45 @@ class MySpecies(Simon):
             else:
                 self.flee()
 
+    def flee(self):
+        self.flee_1()
 
+    def flee_1(self):
+        predators = [p for p in self.visible_simons if self._is_predator(p)]
+        danger_arcs = []
+        biggest_arc = 0
+        for rho, pred in predators:
+            phi = util.rel_phi(self.loc, pred.loc)
+            arc = self._threat_eval(pred) / (rho ** (3.0/2))
+            danger_arcs.append(phi, arc)
+            biggest_arc = max(biggest_arc, arc)
+        if biggest_arc >= pi:
+            scale = pi/biggest_arc - util.epsilon    # scale down largest interval to at most 2pi-2ep
+        else:
+            scale = 1
+        safe_intervals = []
+        while not safe_intervals:
+            intervals = [(phi-desperation*arc, phi+desperation*arc) for phi,arc in danger_arcs]
+            safe_intervals = util.subtract_intervals(intervals)
+            scale *= 0.8
+        right, left = max(safe_intervals, key=lambda i: i[1]-i[0])
+        return right + util.angle(right, left)/2
+
+    def flee_2(self):
+        #normalize sum of interval sizes to ~80% of 2pi
+        danger_arcs = []
+        biggest_arc = 0
+        for rho, pred in predators:
+            phi = util.rel_phi(self.loc, pred.loc)
+            arc = self._threat_eval(pred) / (rho ** (3.0/2))
+            danger_arcs.append(phi, arc)
+            biggest_arc = max(biggest_arc, arc)
+
+        scale = 0.9*pi / sum([arc for _,arc in danger_arcs])    # scale so sum of arcs is 90% of circle
+        intervals = [(phi-desperation*arc, phi+desperation*arc) for phi,arc in danger_arcs]
+        safe_intervals = util.subtract_intervals(intervals)
+        right, left = max(safe_intervals, key=lambda i: i[1]-i[0])
+        return right + util.angle(right, left)/2
 
     def reproduce_sex(self, mate):
         energy_donation = min(self.energy, self.energy_inheritance * self.max_energy)
